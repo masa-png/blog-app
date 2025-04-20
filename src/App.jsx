788363e619884d43c5ddbd64c1727e5f976f24d5
三嶋雅幸 <chicks-job-62@icloud.com>
2025-04-20 11:26:04 +0900

    課題4の実装

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Detail from "./components/posts/Detail";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/posts/:id" element={<Detail />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
